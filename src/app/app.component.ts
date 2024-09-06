8d81fc7c8ab67ff5ed5e93b6ffeba0db3b42d254
jsanabrh04 <daalosx@gmail.com>
2024-09-06 14:00:03 -0500

    piano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'AngularPiano';
+
+  public playSound(note: string) {
+    let audio = new Audio();
+    audio.src = `../assets/sounds/${note}.wav`;
+    audio.load();
+    audio.play();
+  }
 }
